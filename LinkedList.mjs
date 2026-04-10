e2e87a898a2b257864c3d6c9421cb17f9a8dfaa3
yunjion <yunjion36@gmail.com>
2026-04-10 10:58:16 +0900

    🚀자료구조 실습

--- LinkedList.mjs
+++ LinkedList.mjs
@@ -1,7 +1,37 @@
 class Node {
-    constructors(data, next = null) {
-        this.data = data;
+    constructor(data,next = null) {
         this.next = next;
     }
 }
-export {Node};
+
+class LikedList {
+    constructor() {
+        this.head = null;
+        this.count = 0;
+
+
+    }
+insertAt(index,data) {
+    if(index < 0 || index > this.data) {
+            throw new error ("범위를 벗어났습니다");
+        }
+
+        let newNode = new Node(data);
+        if(index === 0) {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        else {
+            let currentNode = this.head;
+            for(let i = 0; i < index-1; i++) {
+                currentNode = currentNode.next;
+            }
+            newNode.next = currentNode.next;
+            currentNode.next = newNode;
+        }
+        this.count++;
+
+
+    }
+}
+export{Node,LikedList}; 
--- test.mjs
+++ test.mjs
@@ -1,4 +1,19 @@
-import { Node } from "./LinkedList.mjs";
-let node1 = new Node(1);
-let node2 = new Node(2);
-let node3 = new Node(3);
+import { LikedList, Node } from "./LinkedList.mjs";
+// let node1 = new Node(1);
+// let node2 = new Node(2);
+// let node3 = new Node(3);
+
+// node1.next = node2;
+// node2.next = node3;
+
+// console.log(node1.data);
+// console.log(node2.next.data);
+// console.log(node1.next.next.data);
+let list = new LikedList();
+console.log("==== insertAt() 호출 ====");
+list.insertAt(0,0);
+list.insertAt(1,1);
+list.insertAt(2,2);
+list.insertAt(3,3);
+list.insertAt(4,4);
+
