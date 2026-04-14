e31f679cbdbbaeb73265c38b01425ef716660fa5
yunjion <yunjion36@gmail.com>
2026-04-14 15:22:57 +0900

    자료구조 실습

--- LinkedList.mjs
+++ LinkedList.mjs
@@ -1,58 +1,153 @@
+// Node 클래스
 class Node {
-    constructor(data,next = null) {
+    constructor(data, next = null) {
         this.data = data;
         this.next = next;
     }
 }
 
+// LinkedList 클래스
 class LikedList {
     constructor() {
         this.head = null;
         this.count = 0;
     }
 
-    //printAll
+    // ======================
+    // 전체 출력
+    // ======================
     printAll() {
         let currentNode = this.head;
         let text = "[";
-        while(currentNode != null) {
-            //console.log(currentNode.data);
+
+        while (currentNode != null) {
             text += currentNode.data;
             currentNode = currentNode.next;
-            if(currentNode != null) text += "[";
+
+            if (currentNode != null) text += ",";
         }
+
         text += "]";
         console.log(text);
     }
 
-    //clear
+    // ======================
+    // 초기화
+    // ======================
     clear() {
         this.head = null;
         this.count = 0;
     }
 
-
-insertAt(index,data) {
-   if(index < 0 || index > this.count) {
-    throw new Error("범위를 벗어났습니다");
-}
+    // ======================
+    // index 위치 삽입
+    // ======================
+    insertAt(index, data) {
+        if (index < 0 || index > this.count) {
+            throw new Error("범위를 벗어났습니다");
+        }
 
         let newNode = new Node(data);
-        if(index === 0) {
+
+        // 맨 앞 삽입
+        if (index === 0) {
             newNode.next = this.head;
+            this.head = newNode;
+        }
+        // 중간 삽입
+        else {
+            let currentNode = this.head;
+
+            for (let i = 0; i < index - 1; i++) {
+                currentNode = currentNode.next;
+            }
+            //시험 나옴 
+            newNode.next = currentNode.next;
+            currentNode.next = newNode;
+        }
+
+        this.count++;
+    }
+
+    // ======================
+    // 맨 뒤 삽입
+    // ======================
+    insertLast(data) {
+        let newNode = new Node(data);
+
+        // 비어있을 때
+        if (this.head === null) {
             this.head = newNode;
         }
         else {
             let currentNode = this.head;
-            for(let i = 0; i < index-1; i++) {
+
+            while (currentNode.next !== null) {
                 currentNode = currentNode.next;
             }
-            newNode.next = currentNode.next;
+
             currentNode.next = newNode;
         }
+
         this.count++;
+    }
 
+    // ======================
+    // index 위치 삭제
+    // ======================
+    deleteAt(index) {
+    if (index < 0 || index >= this.count) {
+        throw new Error("범위를 벗어났습니다");
+    }
 
+    let deleteNode;
+
+    // 맨 앞 삭제
+    if (index === 0) {
+        deleteNode = this.head;
+        this.head = this.head.next;
+    }
+    else {
+        let currentNode = this.head;
+
+        for (let i = 0; i < index - 1; i++) {
+            currentNode = currentNode.next;
+        }
+
+        deleteNode = currentNode.next;
+        currentNode.next = deleteNode.next;
+    }
+
+    this.count--;
+    return deleteNode.data;
+}
+
+    deleteLast(){
+        this.deleteAt(this.deleteAt);
+    }
+    // ======================
+    // index 데이터 가져오기
+    // ======================
+    get(index) {
+        if (index < 0 || index >= this.count) {
+            throw new Error("범위를 벗어났습니다");
+        }
+
+        let currentNode = this.head;
+
+        for (let i = 0; i < index; i++) {
+            currentNode = currentNode.next;
+        }
+
+        return currentNode.data;
+    }
+
+    // ======================
+    // 크기 반환
+    // ======================
+    size() {
+        return this.count;
     }
 }
-export{Node,LikedList}; 
+
+export { Node, LikedList };
--- test.mjs
+++ test.mjs
@@ -1,27 +1,61 @@
-import { LikedList, Node } from "./LinkedList.mjs";
-// let node1 = new Node(1);
-// let node2 = new Node(2);
-// let node3 = new Node(3);
+import { LikedList } from "./LinkedList.mjs";
 
-// node1.next = node2;
-// node2.next = node3;
-
-// console.log(node1.data);
-// console.log(node2.next.data);
-// console.log(node1.next.next.data);
 let list = new LikedList();
+
 console.log("==== insertAt() 호출 ====");
 list.insertAt(0,0);
 list.insertAt(1,1);
 list.insertAt(2,2);
 list.insertAt(3,3);
-list.insertAt(4,4);
-list.insertAt(3,99);
+//list.insertAt(4,4);
+//list.insertAt(3,99);
 list.printAll();
+//list.insertAt(5,99);
+list.insertLast(99);
+list.printAll();
+
 console.log("");
+
 console.log("==== clear() 호출 ====");
 list.clear();
 list.printAll();
-console.log("");
-console.log("==== insertLast() 호출 ====");
 
+console.log("");
+
+console.log("=== insertLast() 호출 ===");
+list.insertLast(77);
+list.insertLast(78);
+list.insertLast(79);
+list.insertLast(80);
+list.printAll();
+
+
+console.log("");
+
+console.log("==== deleteAt() 호출 ====");
+list.deleteAt(2);
+list.printAll();
+list.insertLast(81);
+list.printAll();
+//list.deleteAt(3);
+list.deleteLast(3);
+list.printAll();
+
+// console.log("==== removeAt() 호출 ====");
+// list.removeAt(1);
+// list.printAll();
+
+// console.log("");
+
+// console.log("==== find() 호출 ====");
+// console.log(list.find(30));
+
+// console.log("");
+
+// console.log("==== get() 호출 ====");
+// console.log(list.get(1));
+
+// console.log("");
+
+// console.log("==== size() 호출 ====");
+// console.log(list.size());
