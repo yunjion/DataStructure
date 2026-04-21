9c6204518dd1bf901376d66ae5841583dd65f421
yunjion <214058896+yunjion@users.noreply.github.com>
2026-04-21 15:32:25 +0900

    "Stack"

---
+++ test_stack.mjs
@@ -0,0 +1,16 @@
+let stack = [];
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.push(4);
+stack.push(5);
+console.log(stack);
+
+stack.pop();
+console.log(stack);
+stack.pop();
+console.log(stack);
+
+let node = stack.pop();
+console.log(node);
+console.log(stack);
